8d770c8c2c58f7138d738014027bcf2311ee0b0b
Francis Hamzagic <francishamzagic@gmail.com>
2024-04-05 13:51:11 -0300

    first example

---
+++ login.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const router = express.Router()
+
+module.exports = () => {
+  const customRouter = new SignupRouter()
+  router.post('/signup', ExpressRouterAdapter.adapt(customRouter))
+}
+
+class ExpressRouterAdapter {
+  static adapt (router) {
+    return async (req, res) => {
+      const httpRequest = {
+        body: req.body
+      }
+      const httpResponse = await router.route(httpRequest)
+      res.status(httpResponse.statusCode).json(httpResponse)
+    }
+  }
+}
+
+// file signup-router
+class SignupRouter {
+  async route (httpRequest) {
+    const { email, password, repeatPassword } = httpRequest.body
+    const user = new SignupUseCase().signup(email, password, repeatPassword)
+    return {
+      statusCode: 200,
+      body: user
+    }
+  }
+}
+
+// file signup-usecase
+class SignupUseCase {
+  async signup (email, password, repeatPassword) {
+    if (password === repeatPassword) {
+      new AddAccountRepository().add(email, password)
+    }
+  }
+}
+
+// file add-account-repository
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+
+class AddAccountRepository {
+  async add (email, password) {
+    const user = await AccountModel.create({ email, password })
+    return user
+  }
+}
